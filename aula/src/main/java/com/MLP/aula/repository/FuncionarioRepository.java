21c7c59599d4ac170f2085edc60207ec21da1f78
maria <maria.luisa.pavezi.08@gmail.com>
2025-08-28 22:26:03 -0300

    model criado

---
+++ aula/src/main/java/com/MLP/aula/repository/FuncionarioRepository.java
@@ -0,0 +1,16 @@
+package com.MLP.aula.repository;
+
+import com.MLP.aula.model.FuncionarioModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface FuncionarioRepository {
+    private List<FuncionarioModel> funcionarios = new ArrayList<>();
+
+    public void salvar (FuncionarioModel funcionarioModel) {
+        funcionarios.add(funcionarioModel)
+    }
+    public List <FuncionarioModel> listar (FuncionarioModel funcionarioModel);
+
+}
